3a57531654782646b1c081f70d7183541a959561
abdul muqeet <abdularmar@gmail.com>
2022-08-14 16:55:42 +0530

    Header updated

--- src/Components/Common/Header/Header.js
+++ src/Components/Common/Header/Header.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import {  Link } from "react-router-dom";
 import "./Header.css";
+import AbcRoundedIcon from '@mui/icons-material/AbcRounded';
 
 
-const Header= () =>{
+const Header= () =>{  
   return (
   <div className='navbar'>
+    <AbcRoundedIcon style={{fontSize:'60pt',marginLeft:'20px',width:'5%'}}/>
     
-    
+    <div className='navbar1'>
       <Link to="/" style={{textDecoration: 'none',color:"black"}}>Home</Link>
    
       <Link to="/About" style={{textDecoration: 'none',color:"black"}}>About</Link>
     
       <Link to="/Contact" style={{textDecoration: 'none', color:"black"}}>Contact</Link>
+      </div>
 
-      <Link to="/form" style={{textDecoration: 'none', color:"black"}}>Sign In</Link>
-      <Link to="/form" style={{textDecoration: 'none', color:"black"}}>Sign up</Link>
+      <Link to="/form" style={{textDecoration: 'none', color:"black",marginRight:'40px'}}>
+        Register
+      </Link>
+
+      
    
   </div>
   );

df9c34e7d74b7d2010f1d8c17dad122f3863c957
Zuhaib khalifa <zuhaibkhalifa@gmail.com>
2022-08-14 22:50:02 +0530

    proper

--- src/Components/Common/Header/Header.js
+++ src/Components/Common/Header/Header.js
@@ -1,54 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
-import AbcRoundedIcon from '@mui/icons-material/AbcRounded';
+import AbcRoundedIcon from "@mui/icons-material/AbcRounded";
 
-<<<<<<< Updated upstream
-
-const Header= () =>{  
-  return (
-  <div className='navbar'>
-    <AbcRoundedIcon style={{fontSize:'60pt',marginLeft:'20px',width:'5%'}}/>
-    
-    <div className='navbar1'>
-      <Link to="/" style={{textDecoration: 'none',color:"black"}}>Home</Link>
-   
-      <Link to="/About" style={{textDecoration: 'none',color:"black"}}>About</Link>
-    
-      <Link to="/Contact" style={{textDecoration: 'none', color:"black"}}>Contact</Link>
-      </div>
-
-      <Link to="/form" style={{textDecoration: 'none', color:"black",marginRight:'40px'}}>
-        Register
-      </Link>
-
-      
-   
-  </div>
-=======
 const Header = () => {
   return (
     <div className="navbar">
-      <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-        Home
-      </Link>
+      <AbcRoundedIcon
+        style={{ fontSize: "60pt", marginLeft: "20px", width: "5%" }}
+      />
 
-      <Link to="/About" style={{ textDecoration: "none", color: "black" }}>
-        About
-      </Link>
+      <div className="navbar1">
+        <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+          Home
+        </Link>
 
-      <Link to="/Contact" style={{ textDecoration: "none", color: "black" }}>
-        Contact
-      </Link>
+        <Link to="/About" style={{ textDecoration: "none", color: "black" }}>
+          About
+        </Link>
 
-      <Link to="/form" style={{ textDecoration: "none", color: "black" }}>
-        Sign In
-      </Link>
-      <Link to="/form" style={{ textDecoration: "none", color: "black" }}>
-        Sign up
+        <Link to="/Contact" style={{ textDecoration: "none", color: "black" }}>
+          Contact
+        </Link>
+      </div>
+
+      <Link
+        to="/form"
+        style={{ textDecoration: "none", color: "black", marginRight: "40px" }}
+      >
+        Register
       </Link>
     </div>
->>>>>>> Stashed changes
   );
 };
 export default Header;

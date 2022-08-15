a7d5e1ca57431f86323a235a7f4e0ed8b40685b0
Manegar Ahmed <94823827+AhmedM1011@users.noreply.github.com>
2022-08-15 12:36:57 +0530

    form updtated

--- src/Components/Common/Header/Header.js
+++ src/Components/Common/Header/Header.js
@@ -3,35 +3,28 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 import AbcRoundedIcon from "@mui/icons-material/AbcRounded";
 
-const Header = () => {
+
+const Header= () =>{  
   return (
-    <div className="navbar">
-      <AbcRoundedIcon
-        style={{ fontSize: "60pt", marginLeft: "20px", width: "5%" }}
-      />
-
-      <div className="navbar1">
-        <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-          Home
-        </Link>
-
-        <Link to="/About" style={{ textDecoration: "none", color: "black" }}>
-          About
-        </Link>
-
-        <Link to="/Contact" style={{ textDecoration: "none", color: "black" }}>
-          Contact
-        </Link>
+  <div className='navbar'>
+    <AbcRoundedIcon style={{fontSize:'60pt',marginLeft:'20px',width:'5%'}}/>
+    
+    <div className='navbar1'>
+      <Link to="/" style={{textDecoration: 'none',color:"black"}}>Home</Link>
+   
+      <Link to="/About" style={{textDecoration: 'none',color:"black"}}>About</Link>
+    
+      <Link to="/Contact" style={{textDecoration: 'none', color:"black"}}>Contact</Link>
       </div>
 
-      <Link
-        to="/form"
-        style={{ textDecoration: "none", color: "black", marginRight: "40px" }}
-      >
+      <Link to="/form" style={{textDecoration: 'none', color:"black",marginRight:'40px'}}>
         Register
       </Link>
-    </div>
-  );
-};
+  </div>
+  )
+}
+
 export default Header;
+
+
 

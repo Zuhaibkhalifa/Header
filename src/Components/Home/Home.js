9f4305f679a9b52a1d1866d04352dc7eb1b5f26b
Zuhaib khalifa <zuhaibkhalifa@gmail.com>
2022-08-15 14:41:49 +0530

    not

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
+import axios from "axios";
 
 const Home = () => {
+  const [massage, setMassage] = useState(null);
+  useEffect(() => {
+    const curr = axios
+      .get("https://dummy-server123.herokuapp.com/")
+      .then((data) => {
+        setMassage(data.data);
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div>
-      <h1>hello world</h1>
+      {massage && (
+        <div>
+          <h1>{massage.status}</h1>
+          <h2>{massage.data}</h2>
+        </div>
+      )}
     </div>
   );
 };

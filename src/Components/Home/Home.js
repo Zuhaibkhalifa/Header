3cc44cc19eb0f0a1fab752ebf5a943edb9bfacc6
Manegar Ahmed <94823827+AhmedM1011@users.noreply.github.com>
2022-08-17 22:15:19 +0530

    Table reupdated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,14 @@ import { Box, Button, Typography } from "@mui/material";
 import axios from "axios";
 import React from "react";
 import { useState } from "react";
+import {Table,
+TableBody,
+TableCell,
+TableContainer,
+TableHead,
+TableRow,
+Paper,
+} from "@mui/material";
 
 const Home = () => {
 
@@ -17,15 +25,54 @@ const Home = () => {
 
   return(
     <>
-    <Button onClick={submit} >Get</Button>
+    <Button onClick={submit} variant="outlined">Get Data</Button>
+    <Box>
+                <TableContainer component={Paper}>
+                  <Table
+                    sx={{ maxWidth: "100%" }}
+                    size="large"
+                    aria-label="a dense table"
+                  >
+                    <TableHead>
+                      <TableRow>
+                        <TableCell align="left" sx={{display: "none"}}>ID</TableCell>
+                        <TableCell align="center" sx={{width: "30%"}}>Name</TableCell>
+                        <TableCell align="center" sx={{width: "30%"}}>Email</TableCell>
+                        <TableCell align="center" sx={{width: "30%"}}>Message</TableCell>
+                      </TableRow>
+                    </TableHead>
+                  </Table>
+                </TableContainer>
+              </Box>
     {
       table.map((value)=> {
         return (
           <Box>
-            <Typography>{value.name}</Typography>
-            <Typography>{value.email}</Typography>
-            <Typography>{value.message}</Typography><br/>
-          </Box>
+                <TableContainer component={Paper}>
+                  <Table
+                    sx={{ minWidth: "100%" }}
+                    size="medium"
+                    aria-label="a dense table"
+                  >
+                    {/* <TableHead>
+                      <TableRow>
+                      <TableCell align="left">ID</TableCell>
+                        <TableCell align="center">Name</TableCell>
+                        <TableCell align="center">Email</TableCell>
+                        <TableCell align="center">Message</TableCell>
+                      </TableRow>
+                    </TableHead> */}
+                    <TableBody>
+                      <TableRow>
+                        <TableCell align="left" sx={{display: "none"}}>{value._id}</TableCell>
+                        <TableCell align="center" sx={{width: "33%"}}>{value.name}</TableCell>
+                        <TableCell align="center" sx={{width: "35%"}}>{value.email}</TableCell>
+                        <TableCell align="center" sx={{width: "40%"}}>{value.message}</TableCell><br/>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Box>
         )
       })
     }

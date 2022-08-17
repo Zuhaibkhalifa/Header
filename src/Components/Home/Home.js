d409d95c92c688f15f4a114ce02bab0d10f4365c
Manegar Ahmed <94823827+AhmedM1011@users.noreply.github.com>
2022-08-17 15:27:20 +0530

    Table updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,103 +1,36 @@
-import React from "react";
-import { Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import Container from "@mui/material/Container";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import { useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
 import axios from "axios";
-import { Grid } from "@mui/material";
-import "./Home.css";
+import React from "react";
+import { useState } from "react";
 
 const Home = () => {
 
-  const [name, setName] = useState({});
-  const [email, setEmail] = useState({});
-  const [message, setMessage] = useState({});
+  const [table,setTable] = useState([])
 
-  const handleName = (e) => {
-    setName(e.target.value);
-  };
+  const submit = () => {
+    axios.get("https://dummy-server123.herokuapp.com/user")
+    .then((response) =>{
+      console.log(response);
+      setTable(response.data.data)
+    })
+  }
 
-  const handleEmail = (em) => {
-    setEmail(em.target.value);
-  };
-
-  const handleMessage = (msg) => {
-    setMessage(msg.target.value);
-  };
-
-  const handleClick = () => {
-      axios.get("https://dummy-server123.herokuapp.com/",{
-        user_name : name,
-        email : email,
-        test : message
-      } ).then((x) => {
-        console.log(x);
-      },(e) => {
-        console.log(e)
-      })
-  };
-  
-    return(
-        <Container sx={{ position: "relative",  height: "100vh",width: "100%",top:70 }}maxWidth="large">
-            <Grid container spacing={10}>
-                <Grid item sm={6} sx={{backgroundColor: "#8d99ae"}}>
-                <Box className="for">
-                <Typography
-                variant="h5"
-                component="h5"
-                
-              >
-                Enter your details below
-              </Typography>
-            <Box className="inner">
-              <TextField
-                // sx={{ width: "70%"}}
-                id="outlined-basics"
-                label="username"
-                variant="outlined"
-                onChange={handleName}
-              ></TextField>
-              <TextField
-                // sx={{ width: "70%"}}
-                id="outlined-basics"
-                label="Email"
-                variant="outlined"
-                onChange={handleEmail}
-              ></TextField>
-              <TextField
-                // sx={{ width: "70%"}}
-                id="outlined-basics"
-                label="Message"
-                multiline
-                variant="outlined"
-                cols="10"
-                rows="5"
-                onChange={handleMessage}
-              ></TextField>
-              <Button
-                className="btn"
-                variant="outlined"
-                onClick={handleClick}
-                sx={{
-                  width:"30%",
-                  m:"auto",
-                  backgroundColor: "#8d99ae",
-                }}
-              >
-                Submit
-              </Button>
-            </Box>
+  return(
+    <>
+    <Button onClick={submit} >Get</Button>
+    {
+      table.map((value)=> {
+        return (
+          <Box>
+            <Typography>{value.name}</Typography>
+            <Typography>{value.email}</Typography>
+            <Typography>{value.message}</Typography><br/>
           </Box>
-          
-                </Grid>
-                <Grid item sm={6} className="bg">
-
-                </Grid>
-            </Grid>
-        </Container>
-    )
+        )
+      })
+    }
+    </>
+  )
 }
 
 export default Home;
